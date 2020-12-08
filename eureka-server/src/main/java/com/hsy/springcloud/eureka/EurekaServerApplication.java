932eb9d796344da45b56aeeb511cc5d72220d6f5
hansy <827020217@qq.com>
2020-12-08 22:55:52 +0800

    打印数据

--- eureka-server/src/main/java/com/hsy/springcloud/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/hsy/springcloud/eureka/EurekaServerApplication.java
@@ -11,6 +11,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
+
+        System.out.println(111);
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 
